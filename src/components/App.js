da05206ae957907e1d780fa449a0c722931cbca4
Garrett <gcamoin5@gmail.com>
2023-03-14 13:34:14 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Home from "/home/gcamoin/react-hooks-import-export-lab/src/components/Home.js"
+import About from "/home/gcamoin/react-hooks-import-export-lab/src/components/About.js"
+import NavBar from "/home/gcamoin/react-hooks-import-export-lab/src/components/NavBar.js"
+import {username, city, image} from "/home/gcamoin/react-hooks-import-export-lab/src/data/user.js"
 
 function App() {
   return (
